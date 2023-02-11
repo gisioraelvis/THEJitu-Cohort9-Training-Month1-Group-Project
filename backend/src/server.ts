acff9417040dbc1a68da841748c8ac25fb657deb
gisioraelvis <gisioraelvis20@gmail.com>
2023-02-11 13:24:01 +0300

    global error handling middlewares

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authroute from "./router/auth-routes";
+import { notFound, errorHandler } from "./middlewares/global-error.middleware";
 dotenv.config({ path: __dirname + "/../.env" });
 
 dotenv.config();
@@ -12,7 +13,14 @@ app.use(cors());
 app.use(json());
 
 // Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 app.use("/auth", authroute);
+
+// Error handling middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 5500;
