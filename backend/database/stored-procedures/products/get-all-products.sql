973e76e7904291861771f9ab0dd5f3a68c042754
gisioraelvis <gisioraelvis20@gmail.com>
2023-02-12 21:16:54 +0300

    Fetch all products with categories and brands

--- backend/database/stored-procedures/products/get-all-products.sql
+++ backend/database/stored-procedures/products/get-all-products.sql
@@ -1,6 +1,32 @@
-CREATE PROCEDURE usp_GetAllProducts
+-- DROP PROCEDURE IF EXISTS usp_GetAllProductsWithCategoriesAndBrands
+
+CREATE PROCEDURE usp_GetAllProductsWithCategoriesAndBrands
 AS
 BEGIN
-    SELECT *
-    FROM Products;
+    SELECT p.*, COALESCE(categories.categories, '[]') AS categories, COALESCE(brands.brands, '[]') AS brands
+    FROM Products p
+        LEFT JOIN (
+SELECT id AS productId, JSON_QUERY(
+(
+SELECT name
+            FROM Categories c
+                INNER JOIN ProductCategory pc ON c.id = pc.categoryId
+            WHERE pc.productId = products.id
+            FOR JSON PATH
+)
+) AS categories
+        FROM Products products
+) categories ON p.id = categories.productId
+        LEFT JOIN (
+SELECT id AS productId, JSON_QUERY(
+(
+SELECT name
+            FROM Brands b
+                INNER JOIN ProductBrand pb ON b.id = pb.brandId
+            WHERE pb.productId = products.id
+            FOR JSON PATH
+)
+) AS brands
+        FROM Products products
+) brands ON p.id = brands.productId;
 END;
--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -513,35 +513,11 @@ export const updateUserProfileByAdmin = async (
  * @route   GET /api/products
  * @access  Public
  */
-// export const getProducts = async (req, res) => {
-//   const pageSize = 10;
-//   const page = Number(req.query.pageNumber) || 1;
-
-//   const keyword = req.query.keyword
-//     ? {
-//         name: {
-//           $regex: req.query.keyword,
-//           $options: "i",
-//         },
-//       }
-//     : {};
-
-//   const count = await Product.countDocuments({ ...keyword });
-//   const products = await Product.find({ ...keyword })
-//     .limit(pageSize)
-//     .skip(pageSize * (page - 1));
-
-//   res.json({ products, page, pages: Math.ceil(count / pageSize) });
-// };
-
-/**
- * @desc    Fetch all products
- * @route   GET /api/products
- * @access  Public
- */
 export const getAllProducts = async (req: Request, res: Response) => {
   try {
-    const products = await dbUtils.exec("usp_GetAllProducts");
+    const products = await dbUtils.exec(
+      "usp_GetAllProductsWithCategoriesAndBrands"
+    );
 
     if (products.recordset.length > 0) {
       return res.status(200).json(products.recordset);
